c1780fa266400e81ad0f435927087017b1868f4f
ASD-9 <ryan.chavatte@gmail.com>
2025-04-12 15:43:32 +0200

    fix command imports (forget to add whoami & why in list)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,10 +3,14 @@ import { CommandMap } from "../types";
 import clear from "./clear";
 import help from "./help";
 import login from "./login";
+import whoami from "./whoami";
+import why from "./why";
 
 const commands: CommandMap = {
   clear,
   login,
+  whoami,
+  why
 };
 
 commands["help"] = help(commands);
