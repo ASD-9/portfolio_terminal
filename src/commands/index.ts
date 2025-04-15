46d4e652f9a45fd1eebfaefcfcfc6fb35c68c461
ASD-9 <ryan.chavatte@gmail.com>
2025-04-15 18:44:36 +0200

    command skills

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,6 +4,7 @@ import clear from "./clear";
 import help from "./help";
 import about from "./about";
 import contact from "./contact";
+import skills from "./skills";
 import login from "./login";
 import whoami from "./whoami";
 import why from "./why";
@@ -12,6 +13,7 @@ const commands: CommandMap = {
   clear,
   about,
   contact,
+  skills,
   login,
   whoami,
   why
